67b594bb88a1920fb91dac12530602aef17e77af
Brad Christie <brad.christie@gmail.com>
2024-09-11 20:39:07 -0400

    Fix minor bug in calendly

--- src/app/(www)/_components/Calendly.tsx
+++ src/app/(www)/_components/Calendly.tsx
@@ -9,11 +9,13 @@ type Props = {
 
 export function Calendly({ accountId, action }: Props) {
   const calendlyUrl = React.useMemo(() => {
-    const url = `https://calendly.com/${accountId}${action && `/${encodeURIComponent(action)}`}`;
+    console.log(`action: ${action}`);
+    const url = `https://calendly.com/${accountId}${typeof action === 'string' ? `/${encodeURIComponent(action)}` : ''}`;
     const params = [
       'embed_type=Inline',
       'hide_gdpr_banner=1',
       'hide_landing_page_details=1',
+      'embed_domain=1',
     ];
     console.log(`URL: ${[url, '?', params.join('&')].join('')}`);
     return [url, '?', params.join('&')].join('');
@@ -22,7 +24,24 @@ export function Calendly({ accountId, action }: Props) {
   React.useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleMessage = (e: MessageEvent<any>) => {
-      console.log(`Window message: ${JSON.stringify(e)}`);
+      /*
+      e.data = ...
+      { event: "calendly.page_height", payload: { height: "__px" } }
+      { event: "calendly.profile_page_viewed" }
+      { event: "calendly.event_type_viewed" }
+      { event: "calendly.date_and_time_selected" }
+      {
+        event: "calendly.event_scheduled"
+        payload: {
+          event: {
+            uri: "https://api.calendly.com/scheduled_events/{guid}}"
+          },
+          invitee: {
+            uri: "https://api.calendly.com/scheduled_events/{guid}/invitees/{guid}"
+          }
+        }
+      }
+      */
       if (
         e.origin === 'https://calendly.com' &&
         e.data?.event?.indexOf('calendly.') === 0
