2a6ca2dd4d99d709cb7e24d6d13b04fa0e7820e2
Brad Christie <brad.christie@gmail.com>
2024-09-26 21:21:26 -0400

    Fix fullscreen scheduler

--- src/app/(www)/_ui/CalendlyContext.tsx
+++ src/app/(www)/_ui/CalendlyContext.tsx
@@ -70,7 +70,7 @@ const CalendlyContextProvider: React.FC<CalendlyContextProviderProps> = ({
   return (
     <CalendlyContext.Provider value={value}>
       {children}
-      <Modal show={isOpen} onHide={handleHide} scrollable fullscreen="md-down" size="lg">
+      <Modal show={isOpen} onHide={handleHide} scrollable fullscreen>
         <Modal.Header closeButton>Book an Appointment</Modal.Header>
         <Modal.Body>
           <Calendly action={action} />
