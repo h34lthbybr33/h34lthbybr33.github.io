bbfd9785ce1f57e4a59fbfb821757b0068829bdf
Brad Christie <brad.christie@gmail.com>
2024-11-26 16:02:05 -0500

    Include content in sitemap

--- src/app/(www)/sitemap.ts
+++ src/app/(www)/sitemap.ts
@@ -1,25 +1,54 @@
 import { MetadataRoute } from 'next';
-import { buildFullUrl, getBlogUrl } from '@www/_lib/util';
+import { buildFullUrl, getBlogUrl, buildBlogTagUrl, buildBlogUrl } from '@www/_lib/util';
+import { getBlogPostTags, getBlogPosts } from './_data/outstatic';
 
-export default function Sitemap(): MetadataRoute.Sitemap {
-  return [
+export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
+  const now = new Date();
+
+  const sitemap: MetadataRoute.Sitemap = [
     {
       url: buildFullUrl('/'),
-      lastModified: new Date(),
+      lastModified: now,
       changeFrequency: 'daily',
       priority: 0.5,
     },
     {
       url: getBlogUrl(true),
-      lastModified: new Date(),
+      lastModified: now,
       changeFrequency: 'daily',
       priority: 0.5,
     },
     {
       url: buildFullUrl('/faq'),
-      lastModified: new Date(),
+      lastModified: now,
       changeFrequency: 'monthly',
       priority: 0.5,
     },
   ];
+
+  const posts = await getBlogPosts();
+  for (const post of posts) {
+    if (post) {
+      sitemap.push({
+        url: buildBlogUrl(post.slug, true),
+        lastModified: post.publishedAt,
+        changeFrequency: 'daily',
+        priority: 0.75,
+      });
+    }
+  }
+
+  const tags = await getBlogPostTags();
+  for (const tag of tags) {
+    if (tag) {
+      sitemap.push({
+        url: buildBlogTagUrl(tag.value, true),
+        lastModified: now,
+        changeFrequency: 'daily',
+        priority: 0.25,
+      });
+    }
+  }
+
+  return sitemap;
 }
